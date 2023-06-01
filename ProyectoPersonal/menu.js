43a86c5ea046ccef1dcd5984acd185a9eacbef7d
Yair2101 <58870991+yairpro2101@users.noreply.github.com>
2023-06-01 14:29:14 -0600

    avance 9

--- ProyectoPersonal/menu.js
+++ ProyectoPersonal/menu.js
@@ -28,7 +28,33 @@ function crearMenu(){
     </div>
 </nav>
             `;  
+
+    footerClases.innerHTML += `
+            <div class="container-fluid">
+        <div class="row p-3 bg-dark text-white text-center align-items-center">
+          <div class="col-xs-12 col-md-6 col-lg-6"> 
+            <h1>Gracias por pensar en nosotros</h1>
+          </div>
+          <div class="col-xs-12 col-md-6 col-lg-6"> 
+            <h3>Links</h3>
+            <div class="mb-2">
+              <a class="text-muted p-3" href="index.html">Muscle Cars</a>
+              <a class="text-muted p-3" href="listaEstudiantes.html">Estudiantes</a>
+            </div>
+            <div class="mb-2">
+              <a class="text-muted p-3" href="listaProfesor.html">Profesores</a>
+              <a class="text-muted p-3" href="listaGrupo.html">Grupos</a>
+            </div>
+            <div class="mb-2">
+              <a class="text-muted p-3" href="listaCursos.html">Cursos</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+            `; 
 }
+
 
 crearMenu();
           
